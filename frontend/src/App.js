daeecf6466c58ac00d74d356ef0406ee0a2ff183
viguime <jvictormguimaraes@gmail.com>
2020-10-16 17:57:09 -0300

    Set uo props and children

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,21 @@ import React from 'react';
 import Header from './components/Header';
 
 function App(){
-    return <Header/>;
+    return(
+        <>
+            <Header title = "Homepage"/>
+                <ul>
+                    <li>Homepage</li>
+                    <li>Homepage</li>
+                </ul>
+            <Header title = "Projects"/>
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                    <li>Login</li>
+                </ul>
+        </>
+    );
 }
 
 export default App;
