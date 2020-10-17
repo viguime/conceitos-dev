e911bf430b08a5fb6bda964e727f4328c3017af5
viguime <jvictormguimaraes@gmail.com>
2020-10-17 11:38:42 -0300

    Set up projects' handle function and states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 
 function App(){
+    const [projects,setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    function handleAddProject(){
+        setProjects([...projects,`Novo projeto ${Date.now()}`]);
+    }
+
     return(
         <>
-            <Header title = "Homepage"/>
-                <ul>
-                    <li>Homepage</li>
-                    <li>Homepage</li>
-                </ul>
             <Header title = "Projects"/>
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                    <li>Login</li>
-                </ul>
+
+            <ul>
+                {projects.map(project =><li key = {project} >{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}> Adicionar projeto </button>
         </>
     );
 }
