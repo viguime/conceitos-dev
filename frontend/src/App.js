ab7032010e32718e0aad42503dbd5a85266af249
viguime <jvictormguimaraes@gmail.com>
2020-10-17 16:07:42 -0300

    Added styles and image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import React, {useState} from 'react';
+
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
+
 import Header from './components/Header';
 
 function App(){
@@ -11,6 +15,8 @@ function App(){
     return(
         <>
             <Header title = "Projects"/>
+
+            <img width={300} src={backgroundImage}/>
 
             <ul>
                 {projects.map(project =><li key = {project} >{project}</li>)}
