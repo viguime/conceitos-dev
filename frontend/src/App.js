dc126930eb89d3b7155196fead82aa344856b580
viguime <jvictormguimaraes@gmail.com>
2021-02-16 14:46:27 -0300

    create api.js file and axios config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header';
 
 function App(){
     const [projects,setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
 
+    useEffect(()=>{
+        api.get('/projects').then(response=>{console.log(response)});
+    }, []);
+
     function handleAddProject(){
         setProjects([...projects,`Novo projeto ${Date.now()}`]);
+        
+        api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Diego Fernandes"
+        });
     }
 
     return(
         <>
             <Header title = "Projects"/>
 
-            <img width={300} src={backgroundImage}/>
 
             <ul>
                 {projects.map(project =><li key = {project} >{project}</li>)}
