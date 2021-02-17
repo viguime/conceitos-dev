4ab19caa306818e88a7ca5f3cffdfda6d0f839e3
viguime <jvictormguimaraes@gmail.com>
2021-02-17 10:55:34 -0300

    small fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,19 @@ import React, {useState, useEffect} from 'react';
 import api from './services/api';
 
 import './App.css';
-
 import Header from './components/Header';
 
 function App(){
     const [projects,setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
 
     useEffect(()=>{
-        api.get('/projects').then(response=>{console.log(response)});
+        api.get('projects').then(response=>{
+            console.log(response);
+            });
     }, []);
 
     function handleAddProject(){
         setProjects([...projects,`Novo projeto ${Date.now()}`]);
-        
-        api.post('projects', {
-            title: `Novo projeto ${Date.now()}`,
-            owner: "Diego Fernandes"
-        });
     }
 
     return(
